4f398db5baefd1314ce7cf613aa0ab7cea87bb8f
ChristopherZev <christopher_zev@icloud.com>
2019-05-07 13:40:15 -0400

    Updated JSX to reference JS object, cannot call an object as text without .text on the call. Updated variables, buttons, and calls to display what each button is calling a var, func, or obj. Introduced how to call a an object to style a button.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@ const App = () =>{
 
 //Declare a variable or a function like seen below
 //to call the func or variable via the JSX below 
-const buttonText = 'Click Me!';
 
-function getButtonText(){
-	return 'Function Call Submit'; 
+//JS objects cannot be called as text 
+
+// const buttonText = {text: 'Click me'} would need 
+// called as {buttonText.text}
+const varButton = ' Var Button';
+
+const style = {backgroundColor: 'red', color: 'white'};
+const jsObjectButton = {text:'jsObj Button'};
+
+function funcButtonText(){
+	return 'Function Button'; 
 }
 
 //---------------------------------------------------------------
@@ -21,17 +29,26 @@ function getButtonText(){
 	{/* for="name" adds the functionatlity for 
 		clicking on a label to directly go take 
 	user into field "name" is just the ID */}
-  			<label className="label" for="name"> Enter Name: </label>
+  			<label className="label" for="name"> 
+  				Enter Name: 
+  			</label>
   	{/*Must use ClassName instead of class to 
   		avoid collisions with Class keyword in React,
   		may be deprecated in the future*/}
   			<input id="name" type="text"/>
   		{/* custom styling in JSX and call JS variables and functions*/}
   			<button style={{backgroundColor: 'blue', color: 'white'}}>
-  			 {buttonText} </button>
+  			 {varButton} </button>
   				{/*or --------*/}
   			 <button style={{backgroundColor: 'blue', color: 'white'}}>
-  			 {getButtonText()} </button>
+  			 {funcButtonText()} </button>
+
+  		{/*Shows how you can style a button by calling a JS object 
+  			variable*/}
+			<button style={style}>
+  			 {jsObjectButton.text} </button>
+  		
+
 
 
 		
